fd274193942795f5a00d64b8f0824dd3417eabf0
k-tkym <23182248+k-tkym@users.noreply.github.com>
2026-02-27 00:50:45 +0900

    HashRouterに変更してルーティングを設定

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import MemberList from './MemberList.jsx';
 import './index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/list" element={<MemberList />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
